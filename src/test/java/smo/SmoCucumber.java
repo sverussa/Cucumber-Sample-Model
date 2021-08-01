747ea6308c366f21a8c45f453ee991a33f51224b
saulo <sauloverussa@gmail.,com>
2021-08-01 20:02:32 -0300

    Iniciando Projeto Simple Model Teste Cucumber

---
+++ src/test/java/smo/SmoCucumber.java
@@ -0,0 +1,22 @@
+package smo;
+
+import io.cucumber.java.pt.Dado;
+import io.cucumber.java.pt.Entao;
+import io.cucumber.java.pt.Quando;
+
+public class SmoCucumber {
+
+    @Dado("que criei o arquivo corretamente")
+    public void que_criei_o_arquivo_corretamente() {
+
+    }
+    @Quando("executar o arquivo")
+    public void executar_o_arquivo() {
+
+    }
+    @Entao("a executação deve finalizar com sucesso")
+    public void a_executação_deve_finalizar_com_sucesso() {
+
+    }
+
+}
---
+++ src/test/java/smo/smo_cucumber.feature
@@ -0,0 +1,10 @@
+#language: pt
+Funcionalidade:
+  Como um desenvolvedor
+  Eu quero validar o funcionamento do cucumber
+  Para melhor meus testes
+
+Cenario: :
+  Dado que criei o arquivo corretamente
+  Quando executar o arquivo
+  Entao a executação deve finalizar com sucesso
